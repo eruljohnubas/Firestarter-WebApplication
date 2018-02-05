7ebddd4a8cbc148a5d1d39fecb277aa13b804e9c
Erul <erulggubas@yahoo.com>
2018-02-05 23:14:39 +0800

    update registration

--- src/app/registration/shared/register.service.ts
+++ src/app/registration/shared/register.service.ts
@@ -3,12 +3,17 @@ import {AngularFireModule} from 'angularfire2'
 import {Registration} from './registration.model'
 import { database } from 'firebase';
 import { AngularFireList } from 'angularfire2/database/interfaces';
+import { AngularFireDatabase } from 'angularfire2/database';
 @Injectable()
 export class RegisterService {
   registrationList: AngularFireList<any>;
   selectedRegister: Registration = new Registration;
 
 
-  constructor() { }
+  constructor(private firebase :AngularFireDatabase) { }
+  
+  getData(){
+    this.registrationList = this.firebase.list ('register');
+  }
 
 }
