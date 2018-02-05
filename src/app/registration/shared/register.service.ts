8fd5fbbfd58a58c3b7e73f91614a973d6ae3d568
Erul <erulggubas@yahoo.com>
2018-02-05 22:57:07 +0800

    modifiying registration

--- src/app/registration/shared/register.service.ts
+++ src/app/registration/shared/register.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import {AngularFireModule} from 'angularfire2'
+import {Registration} from './registration.model'
+import { database } from 'firebase';
+import { AngularFireList } from 'angularfire2/database/interfaces';
 @Injectable()
 export class RegisterService {
+  registrationList: AngularFireList<any>;
+  selectedRegister: Registration = new Registration;
+
 
   constructor() { }
 
